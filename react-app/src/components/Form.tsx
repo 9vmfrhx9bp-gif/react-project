8a6eba4b0684bef077d4b28d75fe3a6524f7cc51
janis <janis.mueck@gmx.de>
2026-05-17 21:54:54 +0200

    use Form installiert

--- react-app/src/components/Form.tsx
+++ react-app/src/components/Form.tsx
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { FormEvent, useRef, useState } from "react";
+import { FieldValues, useForm } from "react-hook-form"
 
 function Form() {
+
+
+    const { register, handleSubmit } = useForm();
+    const onSubmit = (data: FieldValues) => console.log(data);
+
     return (
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
             <div className="mb-3">
                 <label htmlFor="name" className="forml-label">Name</label>
-                <input id="name" type="text" className="form-control" />
+                <input {...register('name')} id="name" type="text" className="form-control" />
             </div>
             <div className="mb-3">
                 <label htmlFor="age" className="form-label">Age</label>
-                <input id="age" type="number" className="form-control" />
+                <input {...register('age')} id="age" type="number" className="form-control" />
             </div>
             <button className="btn btn-primary" type="submit">Submit</button>
 
