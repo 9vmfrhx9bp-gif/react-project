381095a3ed1646ab52171036d5dc057b62aa4c33
janis <janis.mueck@gmx.de>
2026-05-18 19:53:01 +0200

    zod installiert

--- react-app/src/components/Form.tsx
+++ react-app/src/components/Form.tsx
@@ -1,17 +1,24 @@
 import React, { FormEvent, useRef, useState } from "react";
 import { FieldValues, useForm } from "react-hook-form"
 
+interface FormData{
+    name:string;
+    age:number;
+}
+
 function Form() {
 
 
-    const { register, handleSubmit } = useForm();
+    const { register, handleSubmit, formState: { errors }  } = useForm<FormData>();
     const onSubmit = (data: FieldValues) => console.log(data);
 
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
             <div className="mb-3">
                 <label htmlFor="name" className="forml-label">Name</label>
-                <input {...register('name')} id="name" type="text" className="form-control" />
+                <input {...register('name', { required: true, minLength: 5 })} id="name" type="text" className="form-control" />
+                {errors.name?.type === 'required' && <p class: text-danger>The name field is to short</p>}
+                {errors.age?.type === 'minLength' && <p>The name must be at least 5 characters long</p>}
             </div>
             <div className="mb-3">
                 <label htmlFor="age" className="form-label">Age</label>
